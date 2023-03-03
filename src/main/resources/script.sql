f296e1ffab199ed812c382a561c4f9d3e72e8704
nikpirat <javadeveloper2077@gmail.com>
2023-03-03 14:54:41 +0200

    #commit 4: created database properties and script

---
+++ src/main/resources/script.sql
@@ -0,0 +1,30 @@
+use test;
+create table if not exists `skills`
+(
+    `skill_id`   bigint primary key auto_increment,
+    `skill_name` varchar(40) not null
+);
+
+create table if not exists `developers`
+(
+    `developer_id` bigint primary key auto_increment,
+    `name`         varchar(64) not null
+);
+
+create table if not exists `accounts`
+(
+    `account_id`     bigint primary key auto_increment,
+    `developer_id`   bigint unique not null,
+    `username`       varchar(40)   not null,
+    `account_status` enum ('ACTIVE','BANNED','DELETED') DEFAULT 'ACTIVE',
+
+    foreign key (`developer_id`) references `developers` (`developer_id`)
+);
+
+create table if not exists `developer_skills`
+(
+    `developer_id` bigint not null,
+    `skill_id`     bigint not null,
+
+    foreign key (`developer_id`) references `developers` (`developer_id`)
+);
